7dad4cb804078b77ab2d40b58bc83da70fdf5566
Max Mays <cheeser12@ezen.com>
2014-02-03 20:01:25 -0500

    Modified project structure again...

--- infix_eval/infix_eval.py
+++ infix_eval/infix_eval.py
@@ -1,16 +1,13 @@
 #!/usr/bin/python3
 import sys
 from PyQt5 import QtWidgets
-from ui.main import Ui_MainWindow
+from infix_eval.ui import main_ui
 
 def main():
     app = QtWidgets.QApplication(sys.argv)
     main_window = QtWidgets.QMainWindow() 
-    main_window_ui = Ui_MainWindow()
+    main_window_ui = main_ui.Ui_MainWindow()
     main_window_ui.setupUi(main_window)
     main_window.show()
 
     sys.exit(app.exec_())
-
-if __name__ == '__main__':
-    main()
