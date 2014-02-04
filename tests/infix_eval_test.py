e00a09633fa5b5d18c94a61e98d298d98a91855a
Max Mays <cheeser12@ezen.com>
2014-02-04 13:06:35 -0500

    Added basic unit testing stuff

---
+++ tests/infix_eval_test.py
@@ -0,0 +1,13 @@
+import unittest
+
+import context
+import infix_eval.evaluator as evaluator
+
+class TestEvaluator(unittest.TestCase):
+    def testNumericalResult(self):
+        self.fail("Not implemented yet")
+    def testTreeResult(self):
+        self.fail("Not implemented yet")
+
+if __name__ == "__main__":
+    unittest.main()
