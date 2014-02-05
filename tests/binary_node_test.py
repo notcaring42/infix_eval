9867c42ea9ea2033635d7e1842c9506b99dd963f
Max Mays <cheeser12@ezen.com>
2014-02-05 14:32:36 -0500

    Added test for postfix traversal

--- tests/binary_node_test.py
+++ tests/binary_node_test.py
@@ -4,7 +4,13 @@ from infix_eval.binary_node import BinaryNode
 
 class TestBinaryNode(unittest.TestCase):
     def test_postfix_traverse(self):
-        self.fail("Not implemented yet")
+        tree = BinaryNode(10)
+        tree.right = BinaryNode(15)
+        tree.left = BinaryNode(5)
+        tree.left.left = BinaryNode(3)
+        tree.left.right = BinaryNode(7)
+
+        self.assertEqual(tree.postfix_traverse(), "3 7 5 15 10 ")
 
 if __name__ == '__main__':
     unittest.main()
