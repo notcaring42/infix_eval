334359a7667994a8712eefb509e71abd5d03baae
Max Mays <cheeser12@ezen.com>
2014-02-05 15:30:27 -0500

    Added test for tree output

--- tests/infix_eval_test.py
+++ tests/infix_eval_test.py
@@ -13,7 +13,17 @@ class TestEvaluator(unittest.TestCase):
         self.assertEqual(self.evaluator.evaluate(" 2 +  3").result, 5)
         self.assertEqual(self.evaluator.evaluate("1 / 2").result, 0.5)
     def testTreeResult(self):
-        self.fail("Not implemented yet")
+        tree = self.evaluator.evaluate("8 + 6 * 2 / 3 - 1").tree
+
+        self.assertEqual(tree.data, '-')
+        self.assertEqual(tree.right.data, 1)
+        self.assertEqual(tree.left.data, '+')
+        self.assertEqual(tree.left.left.data, 8)
+        self.assertEqual(tree.left.right.data, '/')
+        self.assertEqual(tree.left.right.right.data, 3)
+        self.assertEqual(tree.left.right.left.data, '*')
+        self.assertEqual(tree.left.right.left.left.data, 6)
+        self.assertEqual(tree.left.right.left.right.data, 2)
 
 if __name__ == "__main__":
     unittest.main()
