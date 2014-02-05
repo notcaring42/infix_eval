0c80323173c923fe2d0a0bef5969d71bcc08addb
Max Mays <cheeser12@ezen.com>
2014-02-05 15:20:11 -0500

    Evaluator now returns numerical result and expression tree

--- infix_eval/evaluator.py
+++ infix_eval/evaluator.py
@@ -1,9 +1,11 @@
 import re
+import collections
 
 from infix_eval.binary_node import BinaryNode
 
 class Evaluator(object):
     def __init__(self):
+        self.Result = collections.namedtuple('Result', ['result', 'tree'])
         self.operators = {'+': 0, '-': 0, '*': 1, '/': 1}
 
     def is_num(self, num):
@@ -14,21 +16,38 @@ class Evaluator(object):
 
         return True
 
-    def _pop_operands(self, op, node_stack, operator_stack):
-        operand1 = node_stack.pop()
-        operand2 = node_stack.pop()
+    def _calc(self, op, operand1, operand2):
+        if op == "+":
+            return operand1 + operand2
+        elif op == "-":
+            return operand1 - operand2
+        elif op == "/":
+            return operand1 / operand2
+        elif op == "*":
+            return operand1 * operand2
+        else:
+            raise ArgumentError("Operator invalid")
+
+    def _pop_operands(self, op, node_stack, operator_stack, operand_stack):
+        operand_node2 = node_stack.pop()
+        operand_node1 = node_stack.pop()
 
         node = BinaryNode(op)
-        node.left = operand2
-        node.right = operand1
+        node.left = operand_node1
+        node.right = operand_node2
         node_stack.append(node)
 
+        operand2 = operand_stack.pop()
+        operand1 = operand_stack.pop()
+        operand_stack.append(self._calc(op, operand1, operand2))
+        
     def evaluate(self, infix):
         infix = infix.strip()
         tokens = re.split(r' +', infix)
 
         operator_stack = []
         node_stack = []
+        operand_stack = []
 
         for token in tokens:
             if token in self.operators.keys():
@@ -38,7 +57,7 @@ class Evaluator(object):
                     top_op = operator_stack[-1]
                     while (self.operators[top_op] >= self.operators[token]):
                         self._pop_operands(operator_stack.pop(), node_stack, 
-                                           operator_stack)
+                                           operator_stack, operand_stack)
 
                         if len(operator_stack) == 0:
                             break
@@ -47,10 +66,11 @@ class Evaluator(object):
                     operator_stack.append(token)
             elif self.is_num(token):
                 node_stack.append(BinaryNode(float(token)))
+                operand_stack.append(float(token))
             else:
                 raise ValueError("Invalid token: " + str(token))
 
-        for op in operator_stack:
-           self._pop_operands(op, node_stack, operator_stack)            
+        for op in reversed(operator_stack):
+           self._pop_operands(op, node_stack, operator_stack, operand_stack)            
 
-        return node_stack[0]
+        return self.Result(tree=node_stack[0], result=operand_stack[0])
--- tests/infix_eval_test.py
+++ tests/infix_eval_test.py
@@ -6,12 +6,12 @@ class TestEvaluator(unittest.TestCase):
     def setUp(self):
         self.evaluator = Evaluator()
     def testNumericalResult(self):
-        self.assertEqual(self.evaluator.evaluate("2 + 5"), 7)
-        self.assertEqual(self.evaluator.evaluate("3 + 5 * 2"), 13)
-        self.assertEqual(self.evaluator.evaluate("8 * 7 / 2"), 28)
-        self.assertEqual(self.evaluator.evaluate("8 + 6 * 2 / 3 - 1"), 11)
-        self.assertEqual(self.evaluator.evaluate(" 2+  3"), 5)
-        self.assertEqual(self.evaluator.evaluate("1/2"), 0.5)
+        self.assertEqual(self.evaluator.evaluate("2 + 5").result, 7)
+        self.assertEqual(self.evaluator.evaluate("3 + 5 * 2").result, 13)
+        self.assertEqual(self.evaluator.evaluate("8 * 7 / 2").result, 28)
+        self.assertEqual(self.evaluator.evaluate("8 + 6 * 2 / 3 - 1").result, 11)
+        self.assertEqual(self.evaluator.evaluate(" 2 +  3").result, 5)
+        self.assertEqual(self.evaluator.evaluate("1 / 2").result, 0.5)
     def testTreeResult(self):
         self.fail("Not implemented yet")
 
