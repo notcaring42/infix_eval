3bf1bc64f3e460642c70c22aadac73f6bc09ca2e
Max Mays <cheeser12@ezen.com>
2014-02-07 15:12:07 -0500

    Created the basic outline of the GUI

--- infix_eval/ui/main_ui.py
+++ infix_eval/ui/main_ui.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'main.ui'
 #
-# Created: Mon Feb  3 18:36:23 2014
+# Created: Fri Feb  7 15:11:07 2014
 #      by: PyQt5 UI code generator 5.2
 #
 # WARNING! All changes made in this file will be lost!
@@ -12,12 +12,28 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName("MainWindow")
-        MainWindow.resize(800, 600)
+        MainWindow.resize(366, 454)
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.graphicsView = QtWidgets.QGraphicsView(self.centralwidget)
-        self.graphicsView.setGeometry(QtCore.QRect(0, 0, 431, 581))
-        self.graphicsView.setObjectName("graphicsView")
+        self.verticalLayout = QtWidgets.QVBoxLayout(self.centralwidget)
+        self.verticalLayout.setSizeConstraint(QtWidgets.QLayout.SetDefaultConstraint)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.treeView = QtWidgets.QGraphicsView(self.centralwidget)
+        self.treeView.setObjectName("treeView")
+        self.verticalLayout.addWidget(self.treeView)
+        self.formLayout = QtWidgets.QFormLayout()
+        self.formLayout.setObjectName("formLayout")
+        self.expressionLabel = QtWidgets.QLabel(self.centralwidget)
+        self.expressionLabel.setObjectName("expressionLabel")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.LabelRole, self.expressionLabel)
+        self.expressionLineEdit = QtWidgets.QLineEdit(self.centralwidget)
+        self.expressionLineEdit.setText("")
+        self.expressionLineEdit.setObjectName("expressionLineEdit")
+        self.formLayout.setWidget(0, QtWidgets.QFormLayout.FieldRole, self.expressionLineEdit)
+        self.evaluateButton = QtWidgets.QPushButton(self.centralwidget)
+        self.evaluateButton.setObjectName("evaluateButton")
+        self.formLayout.setWidget(1, QtWidgets.QFormLayout.SpanningRole, self.evaluateButton)
+        self.verticalLayout.addLayout(self.formLayout)
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -28,5 +44,7 @@ class Ui_MainWindow(object):
 
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        MainWindow.setWindowTitle(_translate("MainWindow", "Infix Expression Evaluator"))
+        self.expressionLabel.setText(_translate("MainWindow", "Expression:"))
+        self.evaluateButton.setText(_translate("MainWindow", "Evaluate"))
 
