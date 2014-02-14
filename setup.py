b65d0bb2c8bf93523056ecbd58c831102ffc453e
Max Mays <cheeser12@ezen.com>
2014-02-13 19:33:11 -0500

    setup.py now changes target executable name for Win32

--- setup.py
+++ setup.py
@@ -4,11 +4,13 @@ from cx_Freeze import setup, Executable
 build_exe_options = {"excludes": ["tkinter"]}
 
 base = None
+target_name = "infix_eval"
 if sys.platform == "win32":
+    target_name = "InfixEval.exe"
     base = "Win32GUI"
 
 setup(  name = "infix_eval",
         version = "0.1",
         description = "Infix expression evaluator",
         options = {"build_exe": build_exe_options},
-        executables = [Executable("infix_eval/main.py", base=base, targetName="infix_eval")])
+        executables = [Executable("infix_eval/main.py", base=base, targetName=target_name)])
